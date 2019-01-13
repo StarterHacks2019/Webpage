bf30797db9485ef6681e09278fb654906d93e7a4
Nathankhlai <39472876+Nathankhlai@users.noreply.github.com>
2019-01-13 13:35:25 -0500

    Add files via upload

--- Graph.m
+++ Graph.m
@@ -1,13 +1,14 @@
 data = dlmread('file.txt', '\n');
+data2 = nonzeros(data);
 time = 0:1:25;
 
 h=figure;
-plot(time,data,'r');
+plot(time,data2,'r');
 
 title("Luminescence Detected by Sensors");
 xlabel("Time (s)");
 ylabel("Luminescence (lx)");
-ylim([0 200]);
+ylim([0 1000]);
 xlim([0 25]);
 grid on;
 
